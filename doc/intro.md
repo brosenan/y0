d92d715e62164e73a4e6636779a1c97096f05d86
Boaz Rosenan <brosenan@gmail.com>
2024-04-30 20:14:43 +0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to y0
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/y0/core_test.clj
@@ -0,0 +1,7 @@
+(ns y0.core-test
+  (:require [clojure.test :refer :all]
+            [y0.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
