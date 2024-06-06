8bee4ddc2da6780774c489b8ad23a07e8b253683
Boaz Rosenan <brosenan@gmail.com>
2024-06-06 19:29:19 +0300

    Language documentation: Simple predicate

--- test-y0.sh
+++ test-y0.sh
@@ -9,3 +9,5 @@ all_y0_files=$(find $root -name "*.y0")
 modules=$(for file in $all_y0_files; do rel=${file#"$root/"}; echo ${rel%".y0"}; done)
 
 lein run -m y0.main -p "$root" "$modules"
+
+$basedir/create-docs.sh
