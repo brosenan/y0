6deeee1c862c4d5435860a0a85c213a577e4d5ce
Boaz Rosenan <brosenan@gmail.com>
2024-09-06 12:18:18 +0300

    Remove modules.clj

--- test/y0/edn_parser_test.clj
+++ test/y0/edn_parser_test.clj
@@ -13,7 +13,7 @@
 
 ;; $y_0$'s syntax is EDN-based. This module defines a parser for parsing
 ;; EDN-based languages, including $y_0$ itself. It is pluggable to $y_0$'s
-;; [module system](modules.md).
+;; [module system](polyglot_loader.md).
 
 ;; ## Namespace Conversions
 
