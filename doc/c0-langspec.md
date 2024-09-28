d639e6bbdda3e049f64f61f6dda1e09176364903
Boaz Rosenan <brosenan@gmail.com>
2024-09-28 17:08:41 +0300

    langspec: added support to main

---
+++ doc/c0-langspec.md
@@ -0,0 +1,26 @@
+# $c_0$ Language Spec
+
+This is a language spec for the $c_0$ example language.
+
+## Variables
+
+$c_0$ supports variable definitions.
+
+```c
+int foo() {
+    int a = 1;
+}
+```
+```status
+Success
+```
+
+If the assigned value does not match the variable type, an error is reported.
+```c
+int foo() {
+    int a = 1.2;
+}
+```
+```status
+ERROR: Type mismatch. Variable a was given type "int" but was assign value of type "float" in [:func_def "int" foo ...]
+```
--- src/y0/main.clj
+++ src/y0/main.clj
@@ -1,16 +1,19 @@
 (ns y0.main
-  (:require [clojure.term.colors :refer [green red blue]]
+  (:require [clojure.java.io :as io]
+            [clojure.term.colors :refer [blue green red]]
             [clojure.tools.cli :refer [parse-opts]]
+            [edamame.core :as e :refer [parse-string-all]]
             [y0.builtins :refer [add-builtins]]
-            [y0.explanation :refer [code-location explanation-to-str all-unique-locations explanation-expr-to-str]]
-            [y0.polyglot-loader :refer [load-with-deps eval-mstore]]
-            [y0.edn-parser :refer [edn-parser root-module-symbols]]
-            [y0.resolvers :refer [y0-resolver]]
-            [y0.core :refer [y0-symbols]]
-            [y0.status :refer [ok let-s]]
-            [y0.rules :refer [apply-statements]]
             [y0.config :refer [language-map-from-config]]
-            [edamame.core :as e :refer [parse-string-all]])
+            [y0.core :refer [y0-symbols]]
+            [y0.edn-parser :refer [edn-parser root-module-symbols]]
+            [y0.explanation :refer [all-unique-locations code-location
+                                    explanation-expr-to-str explanation-to-str]]
+            [y0.polyglot-loader :refer [eval-mstore load-with-deps]]
+            [y0.resolvers :refer [y0-resolver]]
+            [y0.rules :refer [apply-statements]]
+            [y0.status :refer [let-s ok]]
+            [y0.spec-analyzer :refer [process-lang-spec]])
   (:gen-class))
 
 (def cli-options
@@ -24,6 +27,8 @@
    ["-l" "--language LANG" "The language in which the given module names are written"
     :default "y0"
     :parse-fn identity]
+   ["-s" "--language-spec" "If this is set, then the positional arguments are expected to be paths to markdown files, to be evaluated as language-specs"]
+   ["-u" "--update-language-spec" "Same as -s, but will also update the markdown files with current statuses"]
    ["-h" "--help"]])
 
 (defn render-location [location]
@@ -33,7 +38,52 @@
           row (quot start 1000000)]
       (str (:path location) ":" row ": "))))
 
-(defn- main [modules {:keys [path language language-config]}]
+(defn- print-error [status]
+  (let [explanation (:err status)
+        message (explanation-to-str explanation)
+        location (code-location explanation)]
+    (binding [*out* *err*]
+      (println (str (render-location location) (red "Error") ":") message)
+      (vec (for [[term loc] (all-unique-locations explanation)]
+             (println (str (render-location loc) (blue "Note") ":") (explanation-expr-to-str term 10)))))))
+
+(defn- eval-modules [modules language lang-map]
+  (let [modules (for [module modules]
+                    {:lang language
+                     :name module})
+          status (let-s [mstore (load-with-deps modules lang-map)
+                         ps (ok (add-builtins {}))]
+                        (eval-mstore mstore #(apply-statements %2 %1 {}) ps))]
+      (if (:err status)
+        (do
+          (print-error status)
+          (System/exit 1))
+        (println (green "Success")))))
+
+(defn- process-language-specs [files lang lang-map update?]
+  (if (empty? files)
+    nil
+    (let [[file & files] files
+          state (with-open [r (io/reader file)]
+                  (let [lines (line-seq r)
+                        state {:state :init
+                               :lang lang
+                               :langmap lang-map
+                               :generate update?}]
+                    (process-lang-spec state lines)))]
+      (doseq [err (:errors state)]
+        (print-error {:err (:explanation err)}))
+      (println "Number of successes: " (:success state))
+      (when update?
+        (with-open [w (io/writer file)]
+          (doseq [line (:generated state)]
+            (.write w (str line (System/lineSeparator))))))
+      (when (and (not update?)
+               (:errors state))
+        (System/exit 1))
+      (recur files lang lang-map update?))))
+
+(defn- main [modules {:keys [path language language-config language-spec update-language-spec]}]
   (let [lang-map (-> language-config
                      language-map-from-config
                      (merge {"y0" {:parse (edn-parser
@@ -41,23 +91,11 @@
                                            "y0"
                                            [])
                                    :read slurp
-                                   :resolve (y0-resolver path)}}))
-        modules (for [module modules]
-                  {:lang language
-                   :name module})
-        status (let-s [mstore (load-with-deps modules lang-map)
-                       ps (ok (add-builtins {}))]
-                      (eval-mstore mstore #(apply-statements %2 %1 {}) ps))]
-    (if (:err status)
-      (let [explanation (:err status)
-            message (explanation-to-str explanation)
-            location (code-location explanation)]
-        (binding [*out* *err*]
-          (println (str (render-location location) (red "Error") ":") message)
-          (vec (for [[term loc] (all-unique-locations explanation)]
-                 (println (str (render-location loc) (blue "Note") ":") (explanation-expr-to-str term 10)))))
-        (System/exit 1))
-      (println (green "Success")))))
+                                   :resolve (y0-resolver path)}}))]
+    (cond
+      update-language-spec (process-language-specs modules language lang-map true)
+      language-spec (process-language-specs modules language lang-map false)
+      :else (eval-modules modules language lang-map))))
 
 (defn -main [& args]
   (let [cli (parse-opts args cli-options)]
