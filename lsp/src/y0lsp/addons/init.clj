7df9be57ebd1cb11ad19bc1cd539536e7101fb33
Boaz Rosenan <brosenan@gmail.com>
2025-02-15 21:44:29 +0200

    lsp: init sends internal notification

--- lsp/src/y0lsp/addons/init.clj
+++ lsp/src/y0lsp/addons/init.clj
@@ -1,15 +1,18 @@
 (ns y0lsp.addons.init 
   (:require
+   [lsp4clj.server :as server]
    [y0lsp.addon-utils :refer [add-req-handler register-addon]]
-   [y0lsp.server :refer [register-req]]))
+   [y0lsp.server :refer [register-notification register-req]]))
 
 (register-req "initialize" any?)
+(register-notification "y0lsp/initialized")
 (register-addon "init"
                 (comp
                  (->> (fn [{:keys [server-capabilities
-                                   client-capabilities]}
-                           {:keys [capabilities]}]
+                                   client-capabilities] :as ctx}
+                           {:keys [capabilities] :as req}]
                         (reset! client-capabilities  capabilities)
+                        (server/receive-notification "y0lsp/initialized" ctx req)
                         {:capabilities server-capabilities
                          :server-info {:name "y0lsp" :version y0lsp.server/version}})
                       (add-req-handler "initialize"))
--- lsp/test/y0lsp/addons/init_test.clj
+++ lsp/test/y0lsp/addons/init_test.clj
@@ -1,10 +1,11 @@
 (ns y0lsp.addons.init-test
   (:require
    [midje.sweet :refer [fact]]
-   [y0lsp.addon-utils :refer [add-req-handler merge-server-capabilities]]
+   [y0lsp.addon-utils :refer [add-notification-handler add-req-handler
+                              merge-server-capabilities]]
    [y0lsp.addons.init :refer :all]
-   [y0lsp.server :refer [register-req]]
-   [y0lsp.initializer-test :refer [addon-test]]))
+   [y0lsp.initializer-test :refer [addon-test]]
+   [y0lsp.server :refer [register-req]]))
 
 ;; # Initialization Addon
 
@@ -37,4 +38,28 @@
    (send "initialize" {:capabilities {:foo 42}}) =>
    {:capabilities {:this {:server {:does :something}}}
     :server-info {:name "y0lsp" :version y0lsp.server/version}}
-   (send "test/foo" {}) => {:res 42}))
+   (send "test/foo" {}) => {:res 42}
+   (shutdown)))
+
+;; ## Sending a Notification
+
+;; In addition to exchanging capabilities, the `init` addon also sends an
+;; internal notification: `y0lps/initialized`, containing the full `initialize`
+;; request. This is intended so that other addons can receive information from
+;; the client other than the client capabilities.
+
+;; In the following example we create an addon that listens to the
+;; `y0lsp/initialized` notification and updates an atom with its contents. Then
+;; we send an `initialize` request and see that the atom is updated.
+(fact
+ (let [x (atom nil)
+       {:keys [send shutdown]}
+       (addon-test "init"
+                   (->> (fn [_ctx params]
+                          (reset! x params))
+                        (add-notification-handler "y0lsp/initialized")))]
+   (send "initialize" {:capabilities {:foo 42}
+                       :something :else}) => map?
+   @x => {:capabilities {:foo 42}
+          :something :else}
+   (shutdown)))
