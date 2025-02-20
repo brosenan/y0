2b5deb92d57fb6c1959974531751f6d2d58c82ab
brosenan <brosenan@gmail.com>
2025-02-20 20:49:36 +0000

    lsp: add explicit dependencies on addon within addon tests

--- lsp/test/y0lsp/addons/diag_test.clj
+++ lsp/test/y0lsp/addons/diag_test.clj
@@ -1,13 +1,10 @@
 (ns y0lsp.addons.diag-test
   (:require
-   [clojure.java.io :as io]
-   [midje.sweet :refer [fact provided =>]]
-   [y0.resolvers :refer [exists?]]
-   [y0.status :refer [unwrap-status]]
-   [y0lsp.addon-utils :refer [add-req-handler]]
+   [midje.sweet :refer [fact =>]]
    [y0lsp.addons.diag :refer :all]
-   [y0lsp.initializer-test :refer [addon-test]]
-   [y0lsp.server :refer [register-req]]))
+   [y0lsp.addons.init]
+   [y0lsp.addons.docsync]
+   [y0lsp.initializer-test :refer [addon-test]]))
 
 ;; # Diagnostics
 
