2fc86422e84280f1c980e8990031238bca1be008
Boaz Rosenan <brosenan@gmail.com>
2025-02-25 16:40:58 +0200

    lsp: find-def: set spec

--- lsp/src/y0lsp/addons/find_def.clj
+++ lsp/src/y0lsp/addons/find_def.clj
@@ -3,9 +3,11 @@
    [y0lsp.addon-utils :refer [add-node-and-lss-to-doc-pos add-req-handler
                               merge-server-capabilities register-addon]]
    [y0lsp.server :refer [register-req]]
-   [y0lsp.location-utils :refer [node-location]]))
+   [y0lsp.location-utils :refer [node-location]]
+   [lsp4clj.coercer :as coercer]
+   [clojure.spec.alpha :as s]))
 
-(register-req "textDocument/definition" any?)
+(register-req "textDocument/definition" (s/nilable ::coercer/location))
 (register-addon "find-def"
                 (->> {:definition-provider true}
                      merge-server-capabilities)
